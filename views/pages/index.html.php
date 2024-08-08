52c37fd65a578513f161e2be780186d5bffcc70f
heric-hh <erhh.015@gmail.com>
2024-08-08 01:04:07 -0600

    working on hero section for mobile

--- views/pages/index.html.php
+++ views/pages/index.html.php
@@ -1,4 +1,21 @@
 <?php 
   include __DIR__ . "/../layout/header.html.php";
 ?>
+<section id="hero">
+  <div class="hero__container">
+    <div class="hero-section">
+      <h2>Tu Tienda De Electrónica De Confianza</h2>
+      <p>Explora nuestra colección de productos electrónicos de las mejores marcas.</p>
+      <p>Calidad, innovación y precios competitivos en un sólo lugar.</p>
+    </div>
+    <div class="hero-button">
+      <button class="button button-secondary>
+        <a href="/productos">Explorar Tienda</a>
+      </button>
+    </div>
+    <div class="hero-speaker-container">
+      <img src="./views/assets/img/bocina.png" alt="bocina">
+    </div>
+  </div>
+</section>
 
