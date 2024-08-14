ec9db5d3af7ae7cf08cd266ae45488bb5cec494b
heric-hh <erhh.015@gmail.com>
2024-08-13 19:31:34 -0600

    feat: seccion de productos nuevos movil

--- views/pages/index.html.php
+++ views/pages/index.html.php
@@ -19,3 +19,35 @@
   </div>
 </section>
 
+<section id="productos_nuevos">
+  <h3>Productos Nuevos</h3>
+  <div class="pn__container">
+    <p>Explora las últimas innovaciones en electrónica que acaben de llegar.</p>
+    <p>Descubre lo nuevo en tecnología que está marcando tendencia.</p>
+  </div>
+  
+  <div class="pn__card-wrapper">
+    <div class="pn__card-gradient-bg">
+      <div class="pn__card-title-wrapper">
+        <p class="pn__card-title">Control Inalámbrico</p>
+      </div>
+    </div>
+    <img src="./views/assets/img/gamepadSinFondo.png" alt="gamepad">
+  </div>
+
+  <div class="pn_button-wrapper">
+    <button class="pn_button button button-secondary">Explora Más</button>
+  </div>
+</section>
+
+<section id="productos_populares">
+  <h3>Productos Más Populares</h3>
+  <div class="pp__container">
+    <p>Descubre los productos electrónicos que están conquistando a nuestros clientes.</p>
+    <p>
+      Desde los más vendidos hasta los más valorados, explora las opciones favoritas que 
+      no te puedes perder.
+    </p>
+  </div>
+</section>
+
